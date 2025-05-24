1978b36400a028ea50b509bcb6c08175fff6dd9a
Flechazo098 <2558755403@qq.com>
2025-05-24 20:44:27 +0800

    初始提交

--- src/main/java/com/flechazo/jzxlib/mixin/ItemMixin.java
+++ src/main/java/com/flechazo/jzxlib/mixin/ItemMixin.java
@@ -2,6 +2,8 @@ package com.flechazo.jzxlib.mixin;
 
 import com.flechazo.jzxlib.common.extension.SlashBladeItemExtensions;
 import net.minecraft.world.item.Item;
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(Item.class)
